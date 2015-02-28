8b171a45db8d1574fbe0e8194572632a4f860eec
Andy <andysingh@Andys-MacBook-Pro.local>
2015-02-27 16:18:25 -0800

    Add config.h

---
+++ config.h
@@ -0,0 +1,3 @@
+#define LAB12_TEST1	1		// Programming Exercise 1: showShortestPaths
+#define LAB12_TEST2	0		// Programming Exercise 2: hasProperColoring 
+#define LAB12_TEST3	0		// Programming Exercise 3: areAllEven
